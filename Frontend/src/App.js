a3c913d8c85668b9a0df94a77484da92258a0444
Dylan Wikaryjak <wikaryjak.dylan@gmail.com>
2022-09-12 11:24:23 +0200

    présentation, modification à faire de mon coté pour améliorer l'application

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,35 +1,25 @@
 import './App.scss';
-import React, {Component} from "react";
+import React, { Component } from "react";
 import {
   BrowserRouter,
-  Router,
   Route,
   Routes,
-  Link,
-  Outlet
 } from 'react-router-dom';
 import HomeScreen from './Views/HomeScreen/HomeScreen';
-import LoginScreen from './Views/LoginScreen/LoginScreen'
 import SignInScreen from './Views/SignIn/SignInScreen';
-import SignUp from './Views/SignUp/SignUp';
 import { Home } from '@mui/icons-material';
-import {Series} from './Views/HomeScreenListView/Series';
+import { Series } from './Views/HomeScreenListView/Series';
+import TestFAQ from './Views/TestFAQ/TestFAQ';
+import LoginScreen from './Views/LoginScreen/LoginScreen';
 
 
 function App() {
   return (
 
     <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={<LoginScreen />} />
-        <Route path="/connexion" element={<SignInScreen />} />
-        <Route path="/accueil" element={<HomeScreen />} />
-        <Route path='inscription' element={<SignUp/>}/>
-        <Route path="/films" element={<HomeScreen type="films" />} />
-        <Route path="/series" component={<Series/>} />
-      </Routes>
+        <LoginScreen />
     </BrowserRouter>
-    
+
   )
 }
 
